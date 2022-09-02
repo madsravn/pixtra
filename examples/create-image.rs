53d78b8c756613387a4c22bb0b88069d4ff80ee7
Mads Ravn <madsravn@gmail.com>
2022-09-02 14:53:19 +0200

    Two different ways of setting a pixel

--- examples/create-image.rs
+++ examples/create-image.rs
@@ -13,4 +13,21 @@ fn main() {
 
     assert_eq!(color, pixel);
     println!("We found color {} and we expected color {}", pixel, color);
+
+
+    // We can do the same without the _mut modifier
+    let color = Pixel {r: 255, g: 255, b: 0, a: 255};
+
+    // Creates new blank canvas. Blank is white.
+    let canvas = Canvas::new(10, 10);
+
+    // Here is the difference.
+    // We discard the old canvas and use the new one which contains the new pixel
+    let canvas = canvas.set_pixel(5, 5, &color);
+
+    let pixel = canvas.get_pixel(5, 5);
+
+    assert_eq!(color, pixel);
+    println!("We found color {} and we expected color {}", pixel, color);
+
 }
